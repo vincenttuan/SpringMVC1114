6c1aabb1eac1bc98a70ce84e504e7aa1294fb060
MB-teacher <MB-teacher@192.168.1.157>
2020-12-26 12:08:50 +0800

    SpringMVC

--- src/main/java/com/spring/mvc/controller/HelloController.java
+++ src/main/java/com/spring/mvc/controller/HelloController.java
@@ -1,9 +1,11 @@
 package com.spring.mvc.controller;
 
 import java.util.Date;
+import java.util.Optional;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -58,6 +60,43 @@ public class HelloController {
         return String.format("%d + %d = %d", x, y, (x+y));
     }
     
+    @RequestMapping("/area/{r}")
+    @ResponseBody
+    public String area(@PathVariable(name = "r", required = false) Integer r) {
+        if(r == null) {
+            return "r = ?";
+        } else {
+            return Math.pow(r, 2) * Math.PI + "";
+        }
+    }
+    
+    @RequestMapping("/area2/{r}")
+    @ResponseBody
+    public String area2(@PathVariable Optional<Integer> r) {
+        if(r.isPresent()) {
+            return Math.pow(r.get(), 2) * Math.PI + "";
+        } else {
+            return "r = ?";
+        }
+    }
+    
+    @RequestMapping("/bmi")
+    @ResponseBody
+    public String bmi(@RequestParam Optional<Double> h,
+                      @RequestParam Optional<Double> w) {
+        
+        if(!h.isPresent()) {
+            return "h = ?";
+        }
+        if(!w.isPresent()) {
+            return "w = ?";
+        }
+        
+        double bmi = w.get() / Math.pow(h.get()/100, 2);
+        return String.format("%.2f", bmi);
+        
+        
+    }
     
     
 }
