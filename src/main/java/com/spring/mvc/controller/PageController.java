1820a5f713b962c4d91f2d29935912f34698da70
MB-teacher <MB-teacher@192.168.1.157>
2020-12-26 13:59:44 +0800

    SpringMVC

--- src/main/java/com/spring/mvc/controller/PageController.java
+++ src/main/java/com/spring/mvc/controller/PageController.java
@@ -1,10 +1,28 @@
 package com.spring.mvc.controller;
 
+import com.spring.mvc.service.BmiService;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/page")
 public class PageController {
+    
+    @Autowired
+    private BmiService bmiService;
+    
+    @RequestMapping("/bmi")
+    public ModelAndView getBmi(@RequestParam Optional<Double> h, 
+                               @RequestParam Optional<Double> w) {
+        Double bmi = bmiService.calcBmi(h.get(), w.get());
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("bmi", bmi); // 將要給 jsp 的資料放入 Model
+        mv.setViewName("bmi_page"); // 設定 jsp 檔名
+        return mv;
+    }
     
 }
--- src/main/java/com/spring/mvc/service/BmiService.java
+++ src/main/java/com/spring/mvc/service/BmiService.java
@@ -5,4 +5,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class BmiService {
     
+    public Double calcBmi(Double h, Double w) {
+        return w / Math.pow(h/100, 2);
+    }
+    
 }
