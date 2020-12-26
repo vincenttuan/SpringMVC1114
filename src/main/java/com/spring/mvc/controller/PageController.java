59f35ac77cc79b40f91c175e61e218e35186faea
MB-teacher <MB-teacher@192.168.1.157>
2020-12-26 14:07:16 +0800

    SpringMVC

--- src/main/java/com/spring/mvc/controller/PageController.java
+++ src/main/java/com/spring/mvc/controller/PageController.java
@@ -4,6 +4,7 @@ import com.spring.mvc.service.BmiService;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -23,6 +24,15 @@ public class PageController {
         mv.addObject("bmi", bmi); // 將要給 jsp 的資料放入 Model
         mv.setViewName("bmi_page"); // 設定 jsp 檔名
         return mv;
+    }
+    
+    @RequestMapping("/bmi2")
+    // 這裡的 String 指的是回傳 View 的名稱
+    public String getBmi2(@RequestParam Optional<Double> h, 
+                          @RequestParam Optional<Double> w, Model model) {
+        Double bmi = bmiService.calcBmi(h.get(), w.get());
+        model.addAttribute("bmi", bmi); // 將要給 jsp 的資料放入 Model 
+        return "bmi_page"; // 設定 jsp 檔名
     }
     
 }
